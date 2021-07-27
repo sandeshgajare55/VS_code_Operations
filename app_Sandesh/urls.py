e9a7ecb8cc2721ee92487a09f04bd67b6e9ce071
sandeshgajare55 <sandeshgajare35@gmail.com>
2021-07-27 15:11:31 +0530

    done

--- app_Sandesh/urls.py
+++ app_Sandesh/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from .import  views
 
 urlpatterns=[
-path("Home/",views.yamaha,name="Ym"),
-path("Away/",views.tvs,name='tvs')
-
-
-  #  path('', views.yamaha,name=("Yamaha"))
+   # path("Home/", views.yamaha,name="Ym"),
+    #path("Away/", views.tvs,name='tvs'),
+path("",views.home),
+path("add",views.add)
 ]
--- app_Sandesh/views.py
+++ app_Sandesh/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def yamaha(request):
+'''def yamaha(request):
     return HttpResponse("Hi this is App by Sandesh")
 
 def tvs(request):
-    return render(request,'new.html')
+    return render(request,'home.html')
+'''
 
+def home(request):
+    return render(request,"op2.html")
+
+def add(request):
+    v1 = int(request.GET['no1'])
+    v2 = int(request.GET['no2'])
+    res = (v1+v2)
+    return render(request,"op3.html",{"result":res})
